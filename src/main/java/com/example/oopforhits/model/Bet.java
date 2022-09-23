93b2f3ccb595d2fb21b7faa7ec51c25de3ff0582
cyberian <fox4080@gmail.com>
2022-09-23 12:26:55 +0700

    Добавлена модель ставки

--- src/main/java/com/example/oopforhits/model/Bet.java
+++ src/main/java/com/example/oopforhits/model/Bet.java
@@ -1,8 +1,18 @@
 package com.example.oopforhits.model;
 
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.UUID;
+
 /**
  * @author Ivan Vinnichenko
  */
-
+@Entity
+@Table(name = "bets")
 public class Bet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private UUID id;
 }
