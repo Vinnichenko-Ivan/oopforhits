af1907a2ac10cac11eb65e19cb80a4b9751b1e51
cyberian <fox4080@gmail.com>
2022-09-23 09:50:43 +0700

    Добавлен Интерфейс RecordsController

--- src/main/java/com/example/oopforhits/controller/RecordsController.java
+++ src/main/java/com/example/oopforhits/controller/RecordsController.java
@@ -1,8 +1,32 @@
 package com.example.oopforhits.controller;
 
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
 /**
  * @author Ivan Vinnichenko
  */
+@Api
+@RestController
+@RequestMapping("/api")
+public interface RecordsController<T> {
 
-public interface RecordsController {
+    @PostMapping("/add")
+    void add(T item);
+
+    @PostMapping("/change")
+    void change(T item);
+
+    @GetMapping("/get")
+    ArrayList<T> get();
+
+    @GetMapping("/getByUUID")
+    T getByUUID(UUID id);
+
+    @DeleteMapping("/deleteByUUID")
+    void deleteByUUID(UUID id);
+
 }
